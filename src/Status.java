f705e6a138660dbd06d4249e0f28f6e6b3d9f5c7
Veljko Dimic <veljkod122@gmail.com>
2017-03-03 22:21:30 -0500

    Updates to game objects and temporary getPlayer2

--- src/Status.java
+++ src/Status.java
@@ -1,12 +1,27 @@
 import java.util.*;
 
 public class Status {
-	gameObject player;
+	Player player;
 	int numMines;
 	List<Mine> mines = new ArrayList<Mine>();
 	int numOpp;
 	List<Player> opponents = new ArrayList<Player>();
 	int numBomb;
 	List<Bomb> bombs = new ArrayList<Bomb>();
-
+	
+	Status(String in){
+		StringTokenizer st = new StringTokenizer(in);
+		if (st.hasMoreTokens() && st.nextToken().equals("STATUS_OUT")){
+			double x = Double.parseDouble(st.nextToken());
+			double y = Double.parseDouble(st.nextToken());
+			double dx = Double.parseDouble(st.nextToken());
+			double dy = Double.parseDouble(st.nextToken());
+			player = new Player(x, y, dx, dy);
+		}
+		
+	}
+	
+	public Player getPlayer(){
+		return player;
+	}
 }
