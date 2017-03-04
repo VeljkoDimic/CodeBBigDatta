be5c8b8b73d741f5375e977ec0720d7fc31b886d
Adam Paul <pauladam316@gmail.com>
2017-03-04 01:14:00 -0500

    Moveto for veljko

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,18 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 	   Server.update("CONFIGURATIONS");
-	   Server.update("BRAKE");
-	   Server.update("ACCELERATE " +  Math.PI * 11/6 + " 1");
+	   //Server.update("BRAKE");
+	   //Server.update("ACCELERATE " +  Math.PI * 11/6 + " 1");
 	   
-	   //System.out.println(status.x);
-	   for( int i = 0; i < 10000; i++) {
-		   Status status = new Status(Server.update("STATUS"));
-		   moveTo(status.getPlayer().getx(),status.getPlayer().gety(),5000,50);
-	   }
+	    
+		Status status = new Status(Server.update("STATUS"));
+		//System.out.println(status.getPlayer().getx() + "Y IS " + status.getPlayer().gety());
+		
+	    moveTo(status.getPlayer().getx(),status.getPlayer().gety(),5000,10000);
 	   //bomb(Status.x, Status.y);
+	    for (int i = 0; i < 10000; i++) {
+	    	Server.update("STATUS");
+	    }
 	}
 	public static boolean bomb(double x, double y) {
 		String returnMessage = Server.update("BOMB " + x + " " + y);
@@ -21,31 +24,40 @@ public class Main {
 		return true;
 	}
 	public static boolean moveTo(double startX, double startY, double endX, double endY) {
-		Status status = new Status(Server.update("STATUS"));
-		double vx = status.getPlayer().getdx() / 0.025;
-		double vy = status.getPlayer().getdx() / 0.025;		
+		Server.update("BRAKE");
+		try {
+			Thread.sleep(3500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//Status status = new Status(Server.update("STATUS"));
+		//double vx = status.getPlayer().getdx() / 0.025;
+		//double vy = status.getPlayer().getdx() / 0.025;		
 		double dx = endX - startX;
 		double dy = endY - startY;
-		double ax = vx - dx/(Math.pow((Math.pow(dx,2) + Math.pow(dy, 2)),0.5));
-		return true;
-		/*
-		System.out.println("DY IS " + dy + " DX IS " + dx);
+		//double ax = vx - dx/(Math.pow((Math.pow(dx,2) + Math.pow(dy, 2)),0.5));
+		//double ay = -1*vy - dy/(Math.pow((Math.pow(dx,2) + Math.pow(dy, 2)),0.5));
 		if (dx == 0) return false;
 		double angle = Math.atan(Math.abs(dy/dx));
-		System.out.println(angle);
+		
 		if (dx < 0 && dy < 0) {
 			angle += Math.PI;
+			System.out.println("3rd");
 		}
 		if (dx < 0 && dy > 0) {
 			angle += Math.PI * 1/2;
+			System.out.println("2nd");
 		}
 		if (dx > 0 && dy < 0) {
 			angle += Math.PI * 3/2;
+			System.out.println("4th");
 		}
+		System.out.println(dx + " " + dy);
 		String returnMessage = Server.update("ACCELERATE " + angle + " 1");
 		if (returnMessage.contains("ERROR")) return false;
 		return true;
-		*/
 	}
 	
 	
