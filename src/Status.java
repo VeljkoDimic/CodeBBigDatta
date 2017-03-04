5f8ad14291b7faac403f93e1b1e0268d538a55cb
Veljko Dimic <veljkod122@gmail.com>
2017-03-03 23:28:59 -0500

    Updates for gameObjects again

--- src/Status.java
+++ src/Status.java
@@ -18,10 +18,51 @@ public class Status {
 			double dy = Double.parseDouble(st.nextToken());
 			player = new Player(x, y, dx, dy);
 		}
+		if (st.hasMoreTokens() && st.nextToken().equals("MINES")){
+			numMines = Integer.parseInt(st.nextToken());
+		}
+		for (int i = 0; i < numMines; i ++){
+			String temp = st.nextToken();
+			if (isNumeric(temp)){
+				double x = Double.parseDouble(temp);
+				double y = Double.parseDouble(st.nextToken());
+				Mine mine = new Mine(x,y);
+				mines.add(mine);
+			}
+			else{
+				double x = Double.parseDouble(st.nextToken());
+				double y = Double.parseDouble(st.nextToken());
+				Mine mine = new Mine(x,y,temp);
+				mines.add(mine);
+			}
+		}
+		
+		if (st.hasMoreTokens() && st.nextToken().equals("PLAYERS")){
+			numOpp = Integer.parseInt(st.nextToken());
+		}
+		for (int i = 0; i < numOpp; i ++){
+			double x = Double.parseDouble(st.nextToken());
+			double y = Double.parseDouble(st.nextToken());
+			double dx = Double.parseDouble(st.nextToken());
+			double dy = Double.parseDouble(st.nextToken());
+			
+		}
+		
+		
 		
 	}
 	
 	public Player getPlayer(){
 		return player;
+	}
+	
+	public static boolean isNumeric(String str){  
+		try{  
+			double d = Double.parseDouble(str);  
+		}  
+		catch(NumberFormatException nfe){  
+			return false;  
+		}  
+		return true;  
 	}
 }
