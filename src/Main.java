c9a1e1e4615dae73a7d1bfa89d19b1187b12c572
Adam Paul <pauladam316@gmail.com>
2017-03-04 00:40:24 -0500

    Updated moveto

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,13 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 	   Server.update("CONFIGURATIONS");
-	   //Server.update("BRAKE");
-	   Server.update("ACCELERATE 0 1");
+	   Server.update("BRAKE");
+	   Server.update("ACCELERATE " +  Math.PI * 11/6 + " 1");
 	   
 	   //System.out.println(status.x);
 	   for( int i = 0; i < 10000; i++) {
 		   Status status = new Status(Server.update("STATUS"));
-		   moveTo(status.x,status.y,9000,9000);
+		   moveTo(status.getPlayer().getx(),status.getPlayer().gety(),5000,50);
 	   }
 	   //bomb(Status.x, Status.y);
 	}
@@ -21,24 +21,31 @@ public class Main {
 		return true;
 	}
 	public static boolean moveTo(double startX, double startY, double endX, double endY) {
+		Status status = new Status(Server.update("STATUS"));
+		double vx = status.getPlayer().getdx() / 0.025;
+		double vy = status.getPlayer().getdx() / 0.025;		
 		double dx = endX - startX;
 		double dy = endY - startY;
+		double ax = vx - dx/(Math.pow((Math.pow(dx,2) + Math.pow(dy, 2)),0.5));
+		return true;
+		/*
+		System.out.println("DY IS " + dy + " DX IS " + dx);
 		if (dx == 0) return false;
-		double angle = Math.atan(dy/dx);
+		double angle = Math.atan(Math.abs(dy/dx));
 		System.out.println(angle);
-		if (dx < 0 && dy > 0) {
-			angle += Math.PI/2;
-		}
 		if (dx < 0 && dy < 0) {
 			angle += Math.PI;
 		}
-		if (dx > 0 && dy > 0) {
+		if (dx < 0 && dy > 0) {
+			angle += Math.PI * 1/2;
+		}
+		if (dx > 0 && dy < 0) {
 			angle += Math.PI * 3/2;
 		}
 		String returnMessage = Server.update("ACCELERATE " + angle + " 1");
 		if (returnMessage.contains("ERROR")) return false;
 		return true;
-		
+		*/
 	}
 	
 	
