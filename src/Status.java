7f5c72bc550173019d27edb7873374ace0b1007c
Veljko Dimic <veljkod122@gmail.com>
2017-03-04 05:11:23 -0500

    Fixed Status

--- src/Status.java
+++ src/Status.java
@@ -17,71 +17,72 @@ public class Status {
 	}
 
 	Status(String in){
-		try {
-			StringTokenizer st = new StringTokenizer(in);
-			if (st.hasMoreTokens() && st.nextToken().equals("STATUS_OUT")){
+		
+		StringTokenizer st = new StringTokenizer(in);
+		if (st.hasMoreTokens()){
+			String temp = st.nextToken();
+			if (temp.equals("STATUS_OUT")){
 				double x = Double.parseDouble(st.nextToken());
 				double y = Double.parseDouble(st.nextToken());
 				double dx = Double.parseDouble(st.nextToken());
 				double dy = Double.parseDouble(st.nextToken());
 				player = new Player(x, y, dx, dy);
 			}
-			if (st.hasMoreTokens() && st.nextToken().equals("MINES")){
-				numMines = Integer.parseInt(st.nextToken());
-				for (int i = 0; i < numMines; i ++){
-					String temp = st.nextToken();
-					if (isNumeric(temp)){
-						double x = Double.parseDouble(temp);
-						double y = Double.parseDouble(st.nextToken());
-						Mine mine = new Mine(x,y);
-						mines.add(mine);
-					}
-					else{
-						double x = Double.parseDouble(st.nextToken());
-						double y = Double.parseDouble(st.nextToken());
-						Mine mine = new Mine(x,y,temp);
-						mines.add(mine);
-					}
+		}
+		if (st.hasMoreTokens() && st.nextToken().equals("MINES")){
+			numMines = Integer.parseInt(st.nextToken());
+			for (int i = 0; i < numMines; i ++){
+				String temp = st.nextToken();
+				if (isNumeric(temp)){
+					double x = Double.parseDouble(temp);
+					double y = Double.parseDouble(st.nextToken());
+					Mine mine = new Mine(x,y);
+					mines.add(mine);
 				}
-			}
-			
-			
-			int count = 0;
-			for (int i = numMines-1; i >= 0; i --){
-				if (mines.get(i).getOwner().equals(Main.USER)){
-					count ++;
-					mines.remove(i);
-				}
-			}
-			numMines -= count;
-			
-			
-			if (st.hasMoreTokens() && st.nextToken().equals("PLAYERS")){
-				numOpp = Integer.parseInt(st.nextToken());
-				for (int i = 0; i < numOpp; i ++){
+				else{
 					double x = Double.parseDouble(st.nextToken());
 					double y = Double.parseDouble(st.nextToken());
-					double dx = Double.parseDouble(st.nextToken());
-					double dy = Double.parseDouble(st.nextToken());
-					Player player = new Player(x,y,dx,dy);
-					opponents.add(player);
-				}
-			}
-			
-			
-			if (st.hasMoreTokens() && st.nextToken().equals("BOMBS")){
-				numBomb = Integer.parseInt(st.nextToken());
-				for (int i = 0; i < numBomb; i ++){
-					double x = Double.parseDouble(st.nextToken());
-					double y = Double.parseDouble(st.nextToken());
-					Bomb bomb = new Bomb(x,y);
-					bombs.add(bomb);
+					Mine mine = new Mine(x,y,temp);
+					mines.add(mine);
 				}
 			}
 		}
-		catch (NullPointerException e) {
-			System.out.println("Null pointer exception");
+		
+		
+		int count = 0;
+		for (int i = numMines-1; i >= 0; i --){
+			if (mines.get(i).getOwner().equals(Main.USER)){
+				count ++;
+				mines.remove(i);
+			}
 		}
+		numMines -= count;
+		
+		
+		if (st.hasMoreTokens() && st.nextToken().equals("PLAYERS")){
+			numOpp = Integer.parseInt(st.nextToken());
+			for (int i = 0; i < numOpp; i ++){
+				double x = Double.parseDouble(st.nextToken());
+				double y = Double.parseDouble(st.nextToken());
+				double dx = Double.parseDouble(st.nextToken());
+				double dy = Double.parseDouble(st.nextToken());
+				Player player = new Player(x,y,dx,dy);
+				opponents.add(player);
+			}
+		}
+		
+		
+		if (st.hasMoreTokens() && st.nextToken().equals("BOMBS")){
+			numBomb = Integer.parseInt(st.nextToken());
+			for (int i = 0; i < numBomb; i ++){
+				double x = Double.parseDouble(st.nextToken());
+				double y = Double.parseDouble(st.nextToken());
+				Bomb bomb = new Bomb(x,y);
+				bombs.add(bomb);
+			}
+		}
+		
+		
 
 	}
 	
